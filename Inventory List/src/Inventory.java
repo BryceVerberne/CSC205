12f9a5c42b89288b17887a957a91b468f0c86d33
BryceVerberne <bryceverberne@gmail.com>
2023-03-15 16:34:06 -0700

    Add Inventory List project

---
+++ Inventory List/src/Inventory.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Inventory {
+    public static void main (String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        InventoryNode headNode;
+        InventoryNode currNode;
+        InventoryNode lastNode;
+
+        String item;
+        int numberOfItems;
+        int i;
+
+        // Front of nodes list
+        headNode = new InventoryNode();
+        lastNode = headNode;
+
+        System.out.print("Size of Inventory: ");
+        int input = scnr.nextInt();
+
+        for(i = 0; i < input; i++ ) {
+            System.out.print("\nItem " + (i + 1) + ": ");
+            item = scnr.next();
+
+            System.out.print(item + " Quantity: ");
+            numberOfItems = scnr.nextInt();
+
+            currNode = new InventoryNode(item, numberOfItems);
+            currNode.insertAtFront(headNode, currNode);
+            lastNode = currNode;
+        }
+
+        // Print linked list
+        currNode = headNode.getNext();
+        while (currNode != null) {
+            currNode.printNodeData();
+            currNode = currNode.getNext();
+        }
+    }
+}
